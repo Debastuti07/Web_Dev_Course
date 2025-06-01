7fa0be801e0ca87ad056947ffb01c83383d30dd8
Debastuti De <debastutide@gmail.com>
2025-06-02 02:58:34 +0530

    Add files via upload

---
+++ JAVASCRIPT/lecture_55/script.js
@@ -0,0 +1,40 @@
+console.log("hey this is tutorial 50");
+var a=5;
+a=a+1
+var b=5;
+var c="tuti"
+console.log(a+b+7);
+console.log(typeof a,typeof b,typeof c);
+{
+    let a=74;
+    console.log(a);
+    
+}
+console.log(a);
+
+//js is case sensitive
+const a1=6;//can't change this value
+// a1=a1+7;//this is invalid
+
+//for mordern js we mostly use "let" variable
+//var has no blocked scope ...it is only globally scoped ..
+
+let x="megha"
+let y=77;
+let z=4.74;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+//type of null is object
+
+let o={
+    "name":"stuti",
+    "job code":7894,
+    "isBeautiful":true
+}
+console.log(o);
+o.salary=10022363;
+console.log(o);
