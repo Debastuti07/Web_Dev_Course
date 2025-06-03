96ab1aa6694bdde3ff6aa64d32c71a185bcc2a85
Debastuti De <debastutide@gmail.com>
2025-06-03 18:59:12 +0530

    Add files via upload

---
+++ JAVASCRIPT/lecture_56/index.js
@@ -0,0 +1,33 @@
+console.log("hello ..i am conditional tutorial");
+
+let age=17;
+let grace=2;
+age+=grace //increment operator
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age**grace);
+/*I
+am 
+a multiline
+comment*/
+if(age==18)
+{
+    console.log("u can drive");
+    
+}
+else if(age==0){
+    console.log("are you kidding?");
+}
+    
+
+else{
+    console.log("u can't drive");
+    
+}
+//ternary operator
+let x=5
+let y=7
+let z=x>y?(x-y):(y-x)
+console.log(z);
