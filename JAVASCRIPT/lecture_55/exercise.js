2312a8f0f7b86e5f2e5da974f389a0b926668a15
Debastuti De <debastutide@gmail.com>
2025-06-04 13:51:28 +0530

    Add files via upload

---
+++ JAVASCRIPT/lecture_55/exercise.js
@@ -0,0 +1,38 @@
+//q1
+
+let numString="7";
+let num=1;
+let sum=(parseInt(numString)+num)
+console.log(sum);
+
+
+//q2
+
+console.log(typeof numString,typeof num,typeof sum);
+
+//q3
+
+const obj={
+    name:"tuti",
+    age:19,
+    stream:"CSE"
+}
+console.log(obj);
+obj.age=20
+console.log(obj);
+//changes possible
+
+//q4
+obj.roll=63;
+console.log(obj);
+//changes possible
+
+//q5
+const dict={
+    "serendipity": "the occurrence of events by chance in a happy or beneficial way",
+  "ephemeral": "lasting for a very short time",
+  "eloquent": "fluent or persuasive in speaking or writing",
+  "zenith": "the highest point; peak",
+  "quintessential": "representing the most perfect or typical example of something"
+}
+console.log(dict);
