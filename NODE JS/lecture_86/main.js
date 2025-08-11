fd885c818073b59e111788c125fe40ef1be50f92
Debastuti De <debastutide@gmail.com>
2025-08-11 12:51:58 +0530

    Add files via upload

---
+++ NODE JS/lecture_86/main.js
@@ -0,0 +1,15 @@
+// const { createServer } = require('node:http');//we import modules with "require"
+
+// import http from "http";
+// import {a,b,d} from "./mymodule.js"
+// console.log(a,b,d);
+
+// import obj from "./mymodule.js"
+// console.log(obj);
+
+
+const a=require("./mymodule2.js");
+console.log(a,__dirname,__filename);
+
+
+
