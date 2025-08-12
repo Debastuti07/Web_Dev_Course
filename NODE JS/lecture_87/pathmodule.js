5be4f0f9322c797ab7b223d20052d11b4468bd82
Debastuti07 <debastutide@gmail.com>
2025-08-12 18:48:46 +0530

    path module

---
+++ NODE JS/lecture_87/pathmodule.js
@@ -0,0 +1,9 @@
+const path=require('path');
+const a=path.basename('C:\\temp\\myfile.html');
+const b=path.dirname('C:\\temp\\myfile.html');
+const c=path.extname('C:\\temp\\myfile.html');
+console.log(a);
+console.log(b);
+console.log(c);
+const d=path.extname(__filename)
+console.log(__filename,d);
