aa4ffb02dd6979293d14741eff6dc3adc516ba57
Debastuti07 <debastutide@gmail.com>
2025-08-20 23:55:02 +0530

    question

---
+++ NODE JS/lecture_91/index.js
@@ -0,0 +1,14 @@
+//you have to write a Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders
+// for example,these files become:
+// 1.name.jpg
+// 2.name.png
+// 3.this.pdf
+// 4.megha.zip
+// 5.Tuti.zip
+// 6.catch.jpg
+
+// this 
+// jpg/name.jpg,jpg/catch.jpg 
+// png/name.png
+// pdf/name.pdf/megha.pdf
+// zip/megha.zip zip/Tuti.zip
