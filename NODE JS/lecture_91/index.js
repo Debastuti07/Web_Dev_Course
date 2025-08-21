2c0852ee0d9634b5f78d902512d0cc77160c2d10
Debastuti07 <debastutide@gmail.com>
2025-08-21 13:58:00 +0530

    question completed

--- NODE JS/lecture_91/index.js
+++ NODE JS/lecture_91/index.js
@@ -12,3 +12,32 @@
 // png/name.png
 // pdf/name.pdf/megha.pdf
 // zip/megha.zip zip/Tuti.zip
+
+
+import fs from "fs/promises";
+import fsn from "fs"
+import path from "path"
+// console.log(fs);
+const basepath="C:\\Users\\DEBASTUTI DE\\Sigma Web Dev Course\\Web-Dev-Course\\NODE JS\\lecture_91"
+
+let files=await fs.readdir(basepath)
+// console.log(files);
+
+
+for (const item of files) {
+    console.log("running for ",item);
+    
+    let ext=item.split(".")[item.split(".").length-1] 
+    if((ext!="js")&&(ext!="json")&&(item.split(".").length-1)) {
+
+        if(fsn.existsSync(path.join(basepath,ext))){
+                  //move the file to the directory if it is not the js and json file
+                  fs.rename(path.join(basepath,item),path.join(basepath,ext,item))
+        }
+        else{
+            fs.mkdir(ext)
+        }
+    }
+    
+    
+}
